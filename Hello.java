6607d5c86f4321e51fbb76418b94a7b53517bec1
jishnugv <jishnu.gv@gmail.com>
2023-05-06 07:09:19 +0000

    Updated Code to build Docker new image

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 class Hello {
     public static void main(String[] args) {
       System.out.println("This is a java app created using Docker");
+
+      Sytem.out.println("updates of code pushed to git hub ");
     }
   }
